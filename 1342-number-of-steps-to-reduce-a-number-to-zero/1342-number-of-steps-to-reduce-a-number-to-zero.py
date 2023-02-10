fc6fd2cd4ef2fb542e796c35c4453c4707ca47dd
Kyu Hyun Leem <90128775+hyunstar11@users.noreply.github.com>
2023-02-11 01:06:50 +0900

    Time: 38 ms (35.75%), Space: 13.8 MB (45.61%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -6,7 +6,7 @@ class Solution:
         
         # 0 이 되기 전까지 반복해주는 게 핵심 
         while num > 0:
-        # 2 로 나누어 떨어진다면 
+        # 2 로 나누어 떨어진다면 // 2 로 나누었을 때 리메인더가 0 이라면?
             if num % 2 == 0:
                 num = num/2
                 count = count + 1
