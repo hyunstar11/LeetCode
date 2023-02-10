2ad122dee77b76815feaed0c9b9b4dc3a3f5c648
Kyu Hyun Leem <90128775+hyunstar11@users.noreply.github.com>
2023-02-11 00:46:13 +0900

    Time: 62 ms (38.73%), Space: 13.8 MB (97.17%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -1,7 +1,7 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        sums = [0] * len(accounts)
-        for i in range(0, len(accounts)):  
-            su = sum(accounts[i])
-            sums.append(su)
-        return max(sums)  #test
+        sums = [0] * len(accounts) # account 길이만큼으로 생성 
+        for i in range(0, len(accounts)):  # account의 길이 만큼 반복 
+            su = sum(accounts[i]) # 각 어카운트의 합을 구함
+            sums.append(su) # 각 합을 sums 에 추가해줌
+        return max(sums)  # sums 에 있는 것 중 가장 큰 값을 찾기 
