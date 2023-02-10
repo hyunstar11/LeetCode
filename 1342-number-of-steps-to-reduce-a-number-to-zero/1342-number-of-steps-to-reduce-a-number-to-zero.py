c1c3828b6612067423d89fe79af8bebf3e943e76
Kyu Hyun Leem <90128775+hyunstar11@users.noreply.github.com>
2023-02-11 01:06:26 +0900

    Time: 26 ms (94.19%), Space: 13.8 MB (45.61%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -1,10 +1,17 @@
 class Solution:
     def numberOfSteps(self, num: int) -> int:
+        
+        # 횟수를 세어주는 count 변수 선언 
         count = 0 
+        
+        # 0 이 되기 전까지 반복해주는 게 핵심 
         while num > 0:
+        # 2 로 나누어 떨어진다면 
             if num % 2 == 0:
                 num = num/2
-                count = count + 1 
+                count = count + 1
+                
+        # 나누어 떨어지지 않는다면? -> 홀수일 시 
             else: 
                 num = num -1 
                 count = count + 1 
