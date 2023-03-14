54c4cd2f41c9352bcb7e9e84c81c66205655a79b
Kyu Hyun Leem <90128775+hyunstar11@users.noreply.github.com>
2023-03-14 14:43:09 +0900

    Time: 27 ms (89.62%), Space: 13.9 MB (10.57%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,7 +1,9 @@
+# Key - Using the find function? 
+
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if needle not in haystack:
-            return -1
+        if needle not in haystack: # 작은 단어가 큰 단어안에 존재하지 않으면 
+            return -1 # -1 반환 
         else: 
-            index = haystack.find(needle)
-            return index 
+            index = haystack.find(needle) # 그렇지 않을 경우에는.. 작은 단어가 큰 단어 안의 어디 (인덱스)에 있는지 find 한다 
+            return index # 그 인덱스 값을 반환한다..  
