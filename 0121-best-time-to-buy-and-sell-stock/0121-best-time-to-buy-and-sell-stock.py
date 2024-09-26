f8b5e8e309209a0646bd0f7984f0193737e11bd7
Kyu Hyun Leem <90128775+hyunstar11@users.noreply.github.com>
2024-09-25 22:30:20 -0500

    Time: 724 ms (82.00%), Space: 20.8 MB (69.47%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,23 +1,16 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        min_price = 9999999
-        max_profit = 0 
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_price = 99999999 ### had issue here 
+        max_profit = 0       ### had issue here 
         
         for price in prices: 
             if price < min_price: 
                 min_price = price
-                #profit = prices[p] - min_price
             elif price - min_price > max_profit:
-                max_profit = price - min_price 
-        return max_profit 
+                max_profit = price - min_price
         
-# my original logic(틀렸을수도 ㅠ)        
-# thinking about the basic logic here 
-# 1. an array is given 
-# 2. find the min and max prices in the array 
-# 3-1. compare the index of the min and max prices 
-# 3-2. if the index of the min > max price, search the next max in the array.. 
-# 3-3. if there is none or if the index of the minimum number is the last digit, then return 0 
-# 4. if there is a next max, then the output should be max - minimum. 
-
-    
+        return max_profit
