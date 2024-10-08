fcbe2d2326f193b1014d606a75465760ca7ca1f6
Andrew K. Leem <90128775+hyunstar11@users.noreply.github.com>
2024-10-08 02:15:04 -0500

    Time: 24 ms (83.86%), Space: 12.3 MB (87.99%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,20 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = re.sub(r'[^A-Za-z0-9]','',s).lower()
-    # s = s.replace(" ","").lower()
-       # s = s.replace(':','')
-       # s = s.replace('[','')
-       # s = s.replace(']','')
-        return s == s[::-1]
-            #return f'"{s}" is a palindrome.'
-        #else: 
-         #   return f'"{s}" is not a palindrome.'
-
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        left, right = 0 , len(s) - 1
         
+        while left < right:
+            if not s[left].isalnum():
+                left += 1 
+            elif not s[right].isalnum():
+                right -= 1
+            elif s[left].lower() != s[right].lower():
+                return False
+            else:
+                left += 1
+                right -= 1
+        return True
+            
